33cc4c052a6e2a9e05ee0a3bd0ad2a57084b5318
jDally987 <jdally987@gmail.com>
2019-04-12 06:31:49 -0400

    Added trio and triotk; need to implement async functionality on window_marketbuy and deribot loops

--- deribot.py
+++ deribot.py
@@ -6,10 +6,12 @@ and execute them in a loop.
 """
 from time import sleep
 import sys
+import trio
+import asyncio
 from deribit_api import RestClient
 
 # interval (in seconds) for run_loop()
-LOOP_INTERVAL = 0.01
+LOOP_INTERVAL = 0.5
 # Position limits (bot won't buy/sell over this amount of contracts)
 MIN_POSITION = -10000
 MAX_POSITION = 10000
@@ -34,7 +36,13 @@ class OrderManager:
         self.client = CLIENT
         self.client.index()
         self.client.account()
-        self.display_positions()
+        # self.display_positions()
+
+    def run(self):
+        try:
+            trio.run(self.run_loop)
+        except KeyboardInterrupt:
+            sys.exit()
 
     def qty(self, amt):
         '''
@@ -55,14 +63,17 @@ class OrderManager:
         self.average_price = position['averagePrice']
         self.liq_price = position['estLiqPrice']
 
-        # return amount
-        print("%d USD " % self.pos_size + ("Long" if position['direction'] == 'buy' else "Short") + " from entry %.2f" % self.average_price)
+        print("%d USD " % self.pos_amt + ("Long" if position['direction'] == 'buy' else "Short") + " from entry %.2f" % self.average_price)
 
     def short_limit_exceeded(self):
         return
 
     def long_limit_exceeded(self):
         return
+
+    def get_highest_bid(self):
+        book = self.client.getorderbook(self.instrument)
+        return book['bids'][0]['price']
 
     def market_buy(self, amt):
         return lambda: self.client.buy(self.instrument, "market", self.qty(amt))
@@ -77,28 +88,38 @@ class OrderManager:
         return self.client.sell(self.instrument, "stop_market", self.qty(amt), stopPx, reduce_only)
 
     def limit_buy(self, amt, price, postOnly=None):
-        return self.client.buy(self.instrument, "limit", self.qty(amt), price, postOnly)
+        return lambda: self.client.buy(self.instrument, "limit", self.qty(amt), price, postOnly)
 
     def limit_sell(self, amt, price, postOnly=None):
-        return self.client.sell(self.instrument, "limit", self.qty(amt), price, postOnly)
-
-    def get_highest_bid(self):
-        book = self.client.getorderbook(self.instrument)
-
-        return book['bids'][0]['price']
+        return lambda: self.client.sell(self.instrument, "limit", self.qty(amt), price, postOnly)
 
     def doomsday_stop_monitor(self):
         return
 
-    def run_loop(self):
+    async def limit_chase_buy(self, amt):
+        startprice = self.get_highest_bid()
+        price = startprice
+        order = self.limit_buy(amt, startprice, postOnly=True)['order']
+        orderid = order['orderId']
+        price = order['price']
+        print(order)
+
+        await self.limit_chaser_loop
+
+    async def limit_chaser_loop(self):
+        print(self.get_highest_bid())
+        await trio.sleep(5)
+        print("ok bid gotten")
+
+    async def run_loop(self):
+        ctr = 0
         while True:
             sys.stdout.write("-----\n")
             sys.stdout.flush()
 
-            sleep(LOOP_INTERVAL)
+            await trio.sleep(LOOP_INTERVAL)
 
             # retrieve position on each loop
             self.display_positions()
-            print(self.get_highest_bid())
-
-            print("lolol")
+            print("lolol ", ctr)
+            ctr = ctr+1
